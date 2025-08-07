d0358122006cf635bc0faceea92e440ce9620db4
Richard <richardmakovs08@gmail.com>
2025-08-07 23:29:08 +0200

    first approach challenge 3

---
+++ challenge_3/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h1 className="text-primary">Hola Bootstrap con React</h1>
+        <button className="btn btn-success">Haz clic</button>
+      </div>
+    </>
+  )
+}
+
+export default App
